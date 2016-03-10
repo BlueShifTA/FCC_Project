df29ffb19825eeafda9436a8aaa9845d6cf3e83a
Surapat <surapat.eki@gmail.com>
2016-03-10 16:30:13 +0700

    Rearrange the directory for ADD

---
+++ Simulation/Signal/ADD/Run_Analysis/Run_many_ADD.sh
@@ -0,0 +1,8 @@
+#./Run_Processes_bash.sh ADD 2 9000 20000 1 1  & 
+#sleep 10
+#./Run_Processes_bash.sh ADD 3 9000 20000 1 1  & 
+#sleep 5
+#./Run_Processes_bash.sh ADD 4 9000 20000 1 1  & 
+#sleep 10
+./Run_Processes_bash.sh ADD 2 12000 12000 1 1  & 
+#./Run_Processes_bash.sh ADD 6 9000 20000 1 1  & 
